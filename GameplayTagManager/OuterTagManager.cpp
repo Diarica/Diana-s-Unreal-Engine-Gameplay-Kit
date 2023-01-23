f8447474acf05355aa773c9bdd1ce1fdc40d3361
BeijingDiana <56779722+BeijingDiana@users.noreply.github.com>
2023-01-23 22:17:36 +0800

    Add files via upload

---
+++ GameplayTagManager/OuterTagManager.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OuterTagManager.h"
+
+#include "FLATLINE/FLATLINECore/GameplayTag/FlatGameplayTag.h"
+
+
+// Sets default values for this component's properties
+UOuterTagManager::UOuterTagManager()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.SetTickFunctionEnable(false);
+
+	// ...
+}
+
+void UOuterTagManager::AddOuterTag(FGameplayTag OuterTag)
+{
+	if(WeaponOuterTagContainer.HasTagExact(OuterTag))
+{
+	return;
+}
+
+	
+	const FGameplayTag OuterParentTag = OuterTagSample.RequestDirectParent();
+	if(OuterTag.RequestDirectParent().MatchesTagExact(OuterParentTag))
+	{
+		WeaponOuterTagContainer.AddTag(OuterTag);
+		OnOuterTagAdded.Broadcast(OuterTag);
+	}
+	
+}
+
+bool UOuterTagManager::HasOuterTag(FGameplayTag OuterTag)
+{
+	return WeaponOuterTagContainer.HasTagExact(OuterTag);
+	
+	
+}
+
+void UOuterTagManager::RemoveOuterTag(FGameplayTag OuterTag)
+{
+	if(WeaponOuterTagContainer.HasTagExact(OuterTag))
+	{
+		WeaponOuterTagContainer.RemoveTag(OuterTag);
+		OnOuterTagRemoved.Broadcast(OuterTag);
+	}
+	 
+	
+}
+
+void UOuterTagManager::RemoveAllOuterTags()
+{
+	WeaponOuterTagContainer.Reset();
+}
+
+
+// Called when the game starts
+void UOuterTagManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
---
+++ GameplayTagManager/OuterTagManager.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "OuterTagManager.generated.h"
+
+
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnOuterTagAdded,FGameplayTag)
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnOuterTagRemoved,FGameplayTag)
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FLATLINE_API UOuterTagManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UOuterTagManager();
+
+	FGameplayTag OuterTagSample;
+
+	void AddOuterTag(FGameplayTag OuterTag);
+
+	bool HasOuterTag(FGameplayTag OuterTag);
+
+	void RemoveOuterTag(FGameplayTag OuterTag);
+
+	void RemoveAllOuterTags();
+	//Call on OnOwnerChanged
+
+
+	FOnOuterTagAdded OnOuterTagAdded;
+	FOnOuterTagRemoved OnOuterTagRemoved;
+
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+private:
+	FGameplayTagContainer WeaponOuterTagContainer;
+};
