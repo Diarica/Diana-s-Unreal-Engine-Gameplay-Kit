ce0f3d67478b50ab87bcce4a592e059ebe3b00d5
BeijingDiana <56779722+BeijingDiana@users.noreply.github.com>
2023-01-23 22:16:30 +0800

    Add files via upload

---
+++ GameplayTagStateMachine/FSMComponent.cpp
@@ -0,0 +1,81 @@
+﻿#include "FSMComponent.h"
+
+
+#include "StateBase.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+// Sets default values for this component's properties
+UFSMComponent::UFSMComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void UFSMComponent::SwitchState(FGameplayTag newStateTag)
+{
+	if (StatesMap[newStateTag] != nullptr)
+	{
+		if (CurrentStateInstance)
+		{
+			CurrentStateInstance->OnStateExit();
+		}
+
+		SwitchOn(newStateTag);
+	}
+}
+
+void UFSMComponent::AddState(FGameplayTag Tag, UStateBase* StateClass)
+{
+	if (StateClass)
+	{
+		for (auto& Element : StatesMap)
+		{
+			if (Element.Key == Tag)
+			{
+				UKismetSystemLibrary::PrintWarning("ERROR : State Tag HAS TWO EXACT SAME");
+				break;
+
+				
+			}
+		}
+	}
+
+	StatesMap.Add(Tag, StateClass);
+}
+
+// Called when the game starts
+void UFSMComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+}
+
+void UFSMComponent::SwitchOn(FGameplayTag newStateTag)
+{
+	if (StatesMap[newStateTag] != nullptr)
+	{
+		CurrentStateInstance = StatesMap[newStateTag];
+		if (CurrentStateInstance)
+		{
+			CurrentStateInstance->OnStateEnter();
+		}
+		CurrentStateTag = newStateTag;
+	}
+}
+
+
+// Called every frame
+void UFSMComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (CurrentStateInstance)
+	{
+		CurrentStateInstance->OnStateUpdate();
+	}
+	// ...
+}
---
+++ GameplayTagStateMachine/TagMessageFSM.cpp
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TagMessageFSM.h"
+
+
+// Sets default values for this component's properties
+UTagMessageFSM::UTagMessageFSM()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+void UTagMessageFSM::ExecuteAddedInnerFuncIfTagValid(FGameplayTag InnerTag)
+{
+	if()
+}
+
+void UTagMessageFSM::ExecuteRemovedInnerFuncIfTagValid(FGameplayTag InnerTag)
+{
+}
+
+void UTagMessageFSM::ExecuteAddedOuterFuncIfTagValid(FGameplayTag OuterTag)
+{
+}
+
+void UTagMessageFSM::ExecuteRemovedOuterFuncIfTagValid(FGameplayTag OuterTag)
+{
+}
+
+
+// Called when the game starts
+void UTagMessageFSM::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UTagMessageFSM::OnReceiveOuterTagAdded(FGameplayTag OuterTagAdded)
+{
+	TFunctionRef<void>* FunctionRef = AddedOuterTagFunctionMap.Find(OuterTagAdded);
+	
+	FunctionRef();
+	
+}
+
+void UTagMessageFSM::OnReceiveInnerTagAdded(FGameplayTag InnerTagAdded)
+{
+	AddedInnerTagFunctionMap.Find(InnerTagAdded);
+}
+
+void UTagMessageFSM::OnReceiveOuterTagRemoved(FGameplayTag OuterTagRemoved)
+{
+	RemovedOuterTagFunctionMap.Find(OuterTagRemoved);
+}
+
+void UTagMessageFSM::OnReceiveInnerTagRemoved(FGameplayTag InnerTagRemoved)
+{
+	RemovedInnerTagFunctionMap.Find(InnerTagRemoved);
+}
+
+void UTagMessageFSM::BindOuterTagAddedFunction(FGameplayTag AddedOuterTag, TFunctionRef<void> TargetFunc)
+{
+	if(IsOuterTag(AddedOuterTag))
+	{
+		AddedOuterTagFunctionMap.Add(AddedOuterTag,TargetFunc);
+	}
+}
+
+void UTagMessageFSM::BindOuterTagRemovedFunction(FGameplayTag RemovedOuterTag, TFunctionRef<void> TargetFunc)
+{
+	if (IsOuterTag(RemovedOuterTag))
+	{
+		RemovedOuterTagFunctionMap.Add(RemovedOuterTag,TargetFunc);
+	}
+}
+
+void UTagMessageFSM::BindInnerTagAddedFunction(FGameplayTag AddedInnerTag, TFunctionRef<void> TargetFunc)
+{
+	if(IsInnerTag(AddedInnerTag))
+	{
+		AddedInnerTagFunctionMap.Add(AddedInnerTag,TargetFunc);
+	}
+}
+
+void UTagMessageFSM::BindInnerTagRemovedFunction(FGameplayTag RemovedInnerTag, TFunctionRef<void> TargetFunc)
+{
+	if(IsInnerTag(RemovedInnerTag))
+	{
+		RemovedInnerTagFunctionMap.Add(RemovedInnerTag,TargetFunc);
+	}
+}
