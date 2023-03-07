253993d3b7df89f610bb81a185ad95fad619ee31
BeijingDiana <56779722+BeijingDiana@users.noreply.github.com>
2023-03-07 21:59:30 +0800

    Add files via upload

---
+++ NetPrediction/ValuePredictComponent.cpp
@@ -0,0 +1,112 @@
+﻿#include "ValuePredictComponent.h"
+
+
+
+
+////
+///COMP MUST REP!
+
+// Sets default values for this component's properties
+UValuePredictComponent::UValuePredictComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	SetIsReplicated(true);
+	// ...
+}
+
+
+void UValuePredictComponent::ReduceValue(int ReduceVal, int MaxValue)
+{
+	CurrentValue = FMath::Clamp(CurrentValue - ReduceVal, 0, MaxValue);
+
+	//CLient Side Prediction
+
+	if (OnValueChange.IsBound())
+	{
+		OnValueChange.Execute(CurrentValue);
+	}
+
+	if (GetOwner()->HasAuthority())
+	{
+		ClientUpdateValue(CurrentValue,ReduceVal,MaxValue);
+	}
+	else
+	{
+		FValueUpdateData ValueUpdateData;
+		ValueUpdateData.ValueDelta = ReduceVal;
+		
+		ValueUpdateDatas.Add(ValueUpdateData);
+	}
+}
+
+void UValuePredictComponent::AddValue(int ValueToAdd, int MaxValue)
+{
+	
+	CurrentValue = FMath::Clamp(CurrentValue + ValueToAdd, 0, MaxValue);
+
+	//CLient Side Prediction
+
+	if (OnValueChange.IsBound())
+	{
+		OnValueChange.Execute(CurrentValue);
+	}
+
+	if (GetOwner()->HasAuthority())
+	{
+		ClientUpdateValue(CurrentValue,ValueToAdd,MaxValue);
+	}
+	else
+	{
+		FValueUpdateData ValueUpdateData;
+		ValueUpdateData.ValueDelta = ValueToAdd;
+		
+		ValueUpdateDatas.Add(ValueUpdateData);
+	}
+}
+
+
+void UValuePredictComponent::ClientUpdateValue_Implementation(int ServerAmmo, int ValueDelta, int MaxVal)
+{
+	if (GetOwner()->HasAuthority())
+	{
+		return;
+	}
+	CurrentValue = ServerAmmo;
+
+	int TotalValueDelta = 0;
+
+	for (int i = 0; i < ValueUpdateDatas.Num(); i++)
+	{
+		TotalValueDelta += ValueUpdateDatas[i].ValueDelta;
+
+		if (ValueUpdateDatas[i].ValueDelta == ValueDelta)
+		{
+			TotalValueDelta -= ValueDelta;
+			ValueUpdateDatas.RemoveAt(i);
+			i--;
+		}
+	}
+
+	CurrentValue = FMath::Clamp(CurrentValue+TotalValueDelta,0,MaxVal);
+
+	if (OnValueChange.IsBound())
+	{
+		OnValueChange.Execute(CurrentValue);
+	}
+}
+
+void UValuePredictComponent::ClientAddValue_Implementation(int AmmoToAdd, int AmmoInMag)
+{
+	if (GetOwner()->HasAuthority())
+	{
+		return;
+	}
+	CurrentValue = FMath::Clamp(CurrentValue + AmmoToAdd, 0, AmmoInMag);
+	if (OnValueChange.IsBound())
+	{
+		OnValueChange.Execute(CurrentValue);
+	}
+}
---
+++ NetPrediction/ValuePredictComponent.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ValuePredictComponent.generated.h"
+USTRUCT()
+struct FValueUpdateData
+{
+	GENERATED_BODY()
+
+	int ValueDelta;
+
+	
+	
+};
+DECLARE_DELEGATE_OneParam(FOnValueChanged, int);
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class UValuePredictComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UValuePredictComponent();
+
+
+	void ReduceValue(int ReduceVal, int MaxValue);
+	void AddValue(int ValueToAdd, int MaxValue);
+
+private:
+	UFUNCTION(Client, Reliable)
+	void ClientUpdateValue(int ServerAmmo, int ValueDelta, int MaxVal);
+
+	UFUNCTION(Client, Reliable)
+	void ClientAddValue(int AmmoToAdd, int AmmoInMag);
+	//The Number of unprocessed server requests value.
+	//incremented in ClientUpdateValue;
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+	int CurrentValue;
+
+TArray<FValueUpdateData> ValueUpdateDatas;
+
+
+#pragma endregion
+
+	FORCEINLINE int GetCurrentValue() const
+	{
+		return CurrentValue;
+	}
+
+	FOnValueChanged OnValueChange;
+};
