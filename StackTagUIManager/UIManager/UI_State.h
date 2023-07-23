81f936a73691a0a1e26a601153194fd35c29639d
BeijingDiana <56779722+BeijingDiana@users.noreply.github.com>
2023-07-23 23:22:23 +0800

    Add files via upload

---
+++ StackTagUIManager/UIManager/UI_State.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FLATLINE/FLATLINECore/Framework/StackStateMachine/StackStateInterface.h"
+#include "UObject/Object.h"
+#include "FLATLINE/FLATLINECore/Framework/GameplayMessageProcessor/GameplayMessageProcessor.h"
+#include "UI_State.generated.h"
+
+
+
+class UGameplayMessageProcessor;
+/**
+ * 
+ */
+UCLASS()
+class FLATLINE_API UUI_State : public UUserWidget,public IStackStateInterface
+{
+	GENERATED_BODY()
+
+
+	virtual void EnterState(EStackAction StackAction) override;
+	virtual void ExitState(EStackAction StackAction) override;
+public:
+
+	
+	virtual void InitUIState(UGameplayMessageProcessor* Processor);
+
+	virtual void BeginDestroy() override;
+
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override; 
+
+	virtual void FinishDestroy() override;
+	
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void K2_InitUIState(UGameplayMessageProcessor* Processor);
+	
+	UFUNCTION(BlueprintNativeEvent)
+	void K2_OnStateEnter(EStackAction StackAction);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void K2_OnStateExit(EStackAction StackAction);
+
+protected:
+	UPROPERTY()
+	UGameplayMessageProcessor* CachedMessageProcessor;
+
+	
+	TArray<FLocalMessageListenerHandle> ListenerHandles;
+};
---
+++ StackTagUIManager/UIManager/UserInterfaceManager.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UserInterfaceManager.h"
+#include "GameFramework/PlayerController.h"
+
+#include "FLATLINE/FLATLINECore/Framework/UIManager/UI_State.h"
+#include "Blueprint/UserWidget.h"
+#include "FLATLINE/FLATLINECore/Framework/GameplayLibHatsuneMiku.h"
+#include "FLATLINE/FLATLINECore/Framework/GameplayMessageProcessor/GameplayMessageProcessor.h"
+
+void UUserInterfaceManager::InitUIStates_Implementation()
+{
+	check(GetOwner())
+	APlayerController* PlayerController = CastChecked<APlayerController>(GetOwner());
+
+	
+	
+	for (auto& Element : StatesToInitialize)
+	{
+		check(Element.Value)
+		if(!Element.Key.IsValid())
+		{
+			return;
+		}
+
+		
+		UUI_State* State = CreateWidget<UUI_State>(PlayerController,Element.Value);
+		check(State)
+		
+		UIInstances.Add(Element.Key,State);
+		State->SetOwningPlayer(PlayerController);
+		State->InitUIState(GetOwner()->FindComponentByClass<UGameplayMessageProcessor>());
+
+		
+	}
+}
+
+UUserInterfaceManager::UUserInterfaceManager()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.SetTickFunctionEnable(false);
+}
+
+UUI_State* UUserInterfaceManager::GetUI(FGameplayTag Tag)
+{
+	if(UUI_State** StatePointer = UIInstances.Find(Tag))
+	{
+		if(*StatePointer)
+		{
+			return *StatePointer;
+		}
+	}
+	return nullptr;
+}
+
+void UUserInterfaceManager::PushUIState(FGameplayTag StateTag)
+{
+	if(!StateTag.IsValid())
+	{
+		return;
+	}
+	
+	if(UUI_State* UI = GetUI(StateTag))
+	{
+		TScriptInterface<IStackStateInterface> Interface(UI);
+		PushState(UI);
+	}
+}
+
+void UUserInterfaceManager::BeginPlay()
+{
+	Super::BeginPlay();
+	if(Cast<APlayerController>(GetOwner())->IsLocalController())
+	{
+		InitUIStates();
+	}
+}
+
+void UUserInterfaceManager::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
