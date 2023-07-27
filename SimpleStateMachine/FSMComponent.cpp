f48c922fc08b7e4bcd84076a75459831cffe0f74
BeijingDiana <56779722+BeijingDiana@users.noreply.github.com>
2023-07-27 17:02:17 +0800

    Add files via upload

---
+++ SimpleStateMachine/FSMComponent.cpp
@@ -0,0 +1,87 @@
+﻿#include "FSMComponent.h"
+
+
+#include "State.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+// Sets default values for this component's properties
+UFSMComponent::UFSMComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void UFSMComponent::SwitchState(int StateID)
+{
+	TScriptInterface<IState> State = StatesMap[StateID];
+	if (State && State->CanEnterState())
+	{
+		if (CurrentStateInstance)
+		{
+			CurrentStateInstance->OnStateExit();
+		}
+
+		SwitchOn(StateID);
+	}
+}
+
+void UFSMComponent::AddState(int StateID, TScriptInterface<IState> StateClass)
+{
+	if (StateClass)
+	{
+		for (auto& Element : StatesMap)
+		{
+			if (Element.Key == StateID)
+			{
+				UKismetSystemLibrary::PrintWarning("ERROR : State Tag HAS TWO EXACT SAME");
+				break;
+			}
+		}
+	}
+
+	StatesMap.Add(StateID, StateClass);
+	++StatesCount;
+}
+
+void UFSMComponent::InitializeFSM()
+{
+}
+
+// Called when the game starts
+void UFSMComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+	InitializeFSM();
+}
+
+void UFSMComponent::SwitchOn(int newStateID)
+{
+	if (StatesMap[newStateID] != nullptr)
+	{
+		CurrentStateInstance = StatesMap[newStateID];
+		if (CurrentStateInstance)
+		{
+			CurrentStateInstance->OnStateEnter();
+		}
+		CurrentStateID = newStateID;
+	}
+}
+
+
+// Called every frame
+void UFSMComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (CurrentStateInstance)
+	{
+		CurrentStateInstance->OnStateUpdate();
+	}
+	// ...
+}
---
+++ SimpleStateMachine/FSMComponent.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+#include "Components/ActorComponent.h"
+
+#include "FSMComponent.generated.h"
+
+
+class IState;
+struct FGameplayTag;
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FLATLINE_API UFSMComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+protected:
+
+	UPROPERTY()
+	TScriptInterface<IState> CurrentStateInstance = nullptr;
+	
+	UPROPERTY()
+	TMap<int,TScriptInterface<IState>> StatesMap;
+
+	int CurrentStateID;
+
+	int StatesCount;
+
+public:
+	// Sets default values for this component's properties
+	UFSMComponent();
+
+	void SwitchState(int StateID);
+
+	void AddState(int StateID, TScriptInterface<IState> StateClass);
+
+	int GetCurrentStateID() const
+	{
+		return CurrentStateID;
+	}
+
+	virtual void InitializeFSM();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void SwitchOn(int newStateID);
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
